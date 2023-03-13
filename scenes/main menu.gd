f97d1c880b7d589a1fcc7e13a01c5d6196a930c7
oschaay25 <126889910+oschaay25@users.noreply.github.com>
2023-03-13 12:55:06 -0400

    buutton

--- scenes/main menu.gd
+++ scenes/main menu.gd
@@ -6,5 +6,6 @@ func _on_playButton_pressed():
 func _on_exitButton_pressed():
 	get_tree().quit()
 
-func _on_playButton_pressed():
-	get_tree().change_scene("res://scenes/main.tscn")
+#This method runs when options button is pressed
+func _on_optionsButton_pressed():
+	get_tree().change_scene("res://scenes/instructions.tscn")
