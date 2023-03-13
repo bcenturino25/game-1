7a77eb0acca4f855549883aa3e32fd9e96bb011f
bcenturino25 <bcenturino25@portergaud.edu>
2023-03-13 12:56:06 -0400

    button code

--- scenes/main menu.gd
+++ scenes/main menu.gd
@@ -6,5 +6,5 @@ func _on_playButton_pressed():
 func _on_exitButton_pressed():
 	get_tree().quit()
 
-func _on_playButton_pressed():
-	get_tree().change_scene("res://scenes/main.tscn")
+func _on_optionsButton_pressed():
+	get_tree().change_scene("res://scenes/instructions.tscn")
